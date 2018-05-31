151d3db08bf4dae27d82f5fb55375a9798ce9896
playola <pol944li@gmail.com>
2018-05-31 19:11:49 +0200

    fix

--- coap-server/resources/res_sensor.c
+++ coap-server/resources/res_sensor.c
@@ -10,7 +10,7 @@
 #include <string.h>
 #include "rest-engine.h"
 #include "er-coap.h"
-#include "../sensor.h"
+#include "libraries/sensor.h"
 /*-------------------------------------------------------*/
 static void res_get_handler(
   void *request,
@@ -37,8 +37,9 @@ static void res_get_handler(
   uint16_t preferred_size,
   int32_t *offset
 ) {
-  unsigned char sensorValue = getSensorValue();
-  printf("Setting sensor value to: %u", sensorValue);
+  unsigned char sensorValue = 0;
+  sensorValue = getSensorValue();
+  printf("Sensor value set to: %u", sensorValue);
   REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
   REST.set_response_payload(response, buffer, snprintf((char *)buffer, preferred_size, sensorValue));
 }
