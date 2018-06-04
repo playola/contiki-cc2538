d69bdcd000dfc68b067556fffd51ff3cb61e74af
playola <pol944li@gmail.com>
2018-06-04 18:43:50 +0200

    added coap-client functionalities

--- coap-client/coap-client.js
+++ coap-client/coap-client.js
@@ -1,3 +1,4 @@
+// Node Coap Client
 const coap = require("node-coap-client").CoapClient;
 
 const observeOptions = {
@@ -11,8 +12,21 @@ const { resource, method, callback, payload, options } = observeOptions;
 
 coap.observe(resource, method, callback, payload, options)
   .then(payload => {
-    console.log("Observe success");
+    console.log("Observe success ", payload);
   });
   .catch(err => {
-    console.log("Observe fail")
+    console.log("Observe fail ", err)
   });
+
+
+// NodeJs Child Process
+const { exec } = require('child_process');
+
+exec('coap get coap://[COAP_IP]:5638/sensors/pressure --observe', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
